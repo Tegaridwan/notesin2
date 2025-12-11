cc084db7fa609744356782c86b59b7e293b8e76b
Tegar Ridwan <redtegar123@gmail.com>
2025-12-11 12:32:14 +0700

    fungsi tampilkan catatan

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,47 @@
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import auth from '@/service/firebaseConfig';
+import auth, { db } from '@/service/firebaseConfig';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { router, useNavigation } from 'expo-router';
+import { useNavigation, useRouter } from 'expo-router';
 import { signOut } from 'firebase/auth';
+import { collection, onSnapshot, orderBy, query, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'; // 1. Tambahkan Text
 import { Alert, FlatList, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
-import { FAB, PaperProvider } from 'react-native-paper';
+import { ActivityIndicator, FAB, PaperProvider } from 'react-native-paper';
 import { Menu, MenuOption, MenuOptions, MenuTrigger } from 'react-native-popup-menu';
 
-const notesData = [
-  { id: 1, title: 'Catatan Pertama', content: 'Ini adalah isi dari catatan pertama saya.' },
-  { id: 2, title: 'Belanja', content: 'Beli susu, roti, dan telur di toko.' },
-  { id: 3, title: 'Ide Proyek', content: 'Membuat aplikasi catatan sederhana menggunakan React Native.' },
-  { id: 4, title: 'Meeting dengan Tim', content: 'Jangan lupa meeting dengan tim pada hari Jumat jam 10 pagi.' },
-  { id: 5, title: 'Liburan', content: 'Rencanakan liburan ke Bali bulan depan.' },
-  { id: 6, title: 'Buku yang Ingin Dibaca', content: 'The Pragmatic Programmer, Clean Code, dan Design Patterns.' },
-  { id: 7, title: 'Resep Masakan', content: 'Coba resep baru untuk membuat pasta carbonara.' },
-  { id: 8, title: 'Olahraga', content: 'Jadwalkan olahraga rutin setiap pagi selama 30 menit.' },
-  { id: 9, title: 'Proyek Sampingan', content: 'Mulai proyek sampingan untuk belajar lebih banyak tentang AI.' },
-  { id: 10, title: 'Catatan Penting', content: 'Ingat untuk selalu backup data penting secara berkala.' },
-];
+// const notesData = [
+//   { id: 1, title: 'Catatan Pertama', content: 'Ini adalah isi dari catatan pertama saya.' },
+//   { id: 2, title: 'Belanja', content: 'Beli susu, roti, dan telur di toko.' },
+//   { id: 3, title: 'Ide Proyek', content: 'Membuat aplikasi catatan sederhana menggunakan React Native.' },
+//   { id: 4, title: 'Meeting dengan Tim', content: 'Jangan lupa meeting dengan tim pada hari Jumat jam 10 pagi.' },
+//   { id: 5, title: 'Liburan', content: 'Rencanakan liburan ke Bali bulan depan.' },
+//   { id: 6, title: 'Buku yang Ingin Dibaca', content: 'The Pragmatic Programmer, Clean Code, dan Design Patterns.' },
+//   { id: 7, title: 'Resep Masakan', content: 'Coba resep baru untuk membuat pasta carbonara.' },
+//   { id: 8, title: 'Olahraga', content: 'Jadwalkan olahraga rutin setiap pagi selama 30 menit.' },
+//   { id: 9, title: 'Proyek Sampingan', content: 'Mulai proyek sampingan untuk belajar lebih banyak tentang AI.' },
+//   { id: 10, title: 'Catatan Penting', content: 'Ingat untuk selalu backup data penting secara berkala.' },
+// ];
+
+interface Note {
+  id: string,
+  title: string,
+  content: string,
+  isPinned: boolean,
+  isArchived: boolean,
+  cretedAt: any,
+  updatedAt: any
+}
 
 export default function HomeScreen() {
   const colorScheme = useColorScheme();
-  const [selectedIds, setSelectedIds] = useState<number[]>([]);
+  const [selectedIds, setSelectedIds] = useState<string[]>([]);
   const isSelectionMode = selectedIds.length > 0;
   const navigation = useNavigation();
+  const router = useRouter();
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleLogout = () => {
     Alert.alert(
@@ -46,6 +61,77 @@ export default function HomeScreen() {
 
   };
 
+useEffect(() => {
+  const unsubscribeAuth = auth.onAuthStateChanged((user) => {
+    if (user) {
+      const q = query(
+        collection(db, 'users', user.uid, 'notes'),
+        where('isTrashed', '==', false),
+        where('isArchived', '==', false),
+        // PERBAIKAN: Tambahkan 'd' menjadi 'updatedAt'
+        orderBy('updatedAt', 'desc') 
+      );
+
+      const unsubscribeSnapshot = onSnapshot(q, (snapshot) => {
+        const notesData: Note[] = [];
+        snapshot.forEach((doc) => {
+          notesData.push({ id: doc.id, ...doc.data() } as Note);
+        });
+
+        const pinned = notesData.filter(n => n.isPinned);
+        const regular = notesData.filter(n => !n.isPinned);
+        setNotes([...pinned, ...regular]);
+        setLoading(false);
+      }, (error) => {
+        console.log("Error Firestore:", error); 
+        setLoading(false);
+      });
+
+      return () => unsubscribeSnapshot();
+    } else {
+      setLoading(false);
+      setNotes([]);
+    }
+  });
+
+  return () => unsubscribeAuth();
+}, []);
+
+  // const renderItem = ({ item }: { item: Note }) => (
+  //   <TouchableOpacity
+  //     style={styles.card}
+  //     onPress={() => router.push({pathname: '/add',
+  //       params: {
+  //         id: item.id,
+  //         title: item.title,
+  //         content: item.content,
+  //         isPinned: item.isPinned.toString(),
+  //         isArchived: item.isArchived.toString()
+  //       }
+  //     })}
+  //   >
+  //     <View style={styles.cardHeader}>
+  //       <Text style={styles.cardTitle} numberOfLines={1}>
+  //         {item.title || "Tanpa Judul"}
+  //       </Text>
+  //       {item.isPinned && <Ionicons name="pin" size={16} color="#4B0082"/>}
+  //     </View>
+  //     <Text style={styles.cardContent} numberOfLines={3}>
+  //       {item.content || "Tidak ada isi teks..."}
+  //     </Text>
+  //     <Text style={styles.cardDate}>
+  //       {item.updateAt?.toDate().toLocaleDateString('id-ID')}
+  //     </Text>
+  //   </TouchableOpacity>
+  // );
+
+  const filteredNotes = notes.filter(note => {
+    const query = searchQuery.toLowerCase();
+    return (
+      note.title.toLocaleLowerCase().includes(query) ||
+      note.content.toLocaleLowerCase().includes(query)
+    )
+  })
 
   useEffect(() => {
     if (isSelectionMode) {
@@ -80,7 +166,8 @@ export default function HomeScreen() {
               </MenuTrigger>
               <MenuOptions customStyles={{ optionsContainer: { marginTop: 30, borderRadius: 10, padding: 5 } }}>
                 <MenuOption onSelect={() => alert('Profile')} text='Profile' />
-                <MenuOption onSelect={ async () => {await signOut(auth);
+                <MenuOption onSelect={async () => {
+                  await signOut(auth);
                   router.replace('/(auth)/signup');
                 }} text='Buat Akun Baru' />
                 <MenuOption onSelect={handleLogout} text='Logout' />
@@ -96,20 +183,35 @@ export default function HomeScreen() {
   }, [navigation, isSelectionMode, selectedIds]
   );
 
-  const handlePress = (id: number) => {
+    const handleLongPress = (id: string) => {
     if (selectedIds.includes(id)) {
-      // Jika ID sudah ada, hapus dari array (unselect)
-      setSelectedIds(selectedIds.filter(selectedId => selectedId !== id));
+      setSelectedIds(selectedIds.filter(item => item !== id));
     } else {
-      // Jika ID belum ada, tambahkan ke array (select)
       setSelectedIds([...selectedIds, id]);
     }
   };
 
-  const handleLongPress = (id: number) => {
-    // Memulai mode seleksi dengan item yang ditekan lama
-    handlePress(id);
+  const handlePress = (id: string) => {
+    if (isSelectionMode) {
+      handleLongPress(id);
+    } else {
+      const item = notes.find(n => n.id === id);
+      if (item) {
+        router.push({
+          pathname: '/add',
+          params: {
+            id: item.id,
+            title: item.title,
+            content: item.content,
+            isPinned: item.isPinned.toString(),
+            isArchived: item.isArchived.toString()
+          }
+        })
+      }
+    }
   };
+
+
 
   return (
     <PaperProvider>
@@ -131,46 +233,63 @@ export default function HomeScreen() {
               placeholder='Cari...'
               style={[styles.searchInput, { color: Colors[colorScheme ?? 'light'].text }]}
               placeholderTextColor={colorScheme === 'dark' ? '#8e8e93' : '#8e8e93'}
+              value={searchQuery}
+              onChangeText={setSearchQuery}
             />
+            {searchQuery.length > 0 && (
+              <Ionicons name="close-circle" size={20} color="8e8e93" onPress={() => setSearchQuery('')} />
+            )}
           </View>
-          <FlatList
-            data={notesData}
-            keyExtractor={(item) => item.id.toString()}
+          {loading ? (
+            <View style={{flex: 1, justifyContent: 'center'}}>
+              <ActivityIndicator size='large' color='#4B0082'/>
+            </View>
+          ): (
+            <FlatList
+            data={filteredNotes}
+            keyExtractor={(item) => item.id}
             contentContainerStyle={{
               paddingTop: 16,
               paddingBottom: 16,
               paddingHorizontal: 10 // Samakan dengan padding container
             }}
+            ListEmptyComponent={
+              <View style={{alignItems: 'center', marginTop: 50}}>
+                <Text style={{color: '#8e8e93'}}>Catatan belum dibuat</Text>
+              </View>
+            }
             renderItem={({ item }) => {
               const isSelected = selectedIds.includes(item.id);
 
               return (
                 <Pressable
                   onLongPress={() => handleLongPress(item.id)}
-                  onPress={() => {
-                    if (isSelectionMode) {
-                      handlePress(item.id);
-                    } else {
-                      // Nanti bisa ditambahkan navigasi ke detail catatan
-                      alert(`Buka detail untuk: ${item.title}`);
-                    }
-                  }}
+                  onPress={() => handlePress(item.id)}
                   style={[
                     styles.cardItem,
                     isSelected ? styles.cardSelected : styles.cardNormal
                   ]}
                 >
                   <View style={styles.cardHeader}>
-                    <Text style={styles.titleText}>{item.title}</Text>
-                    {isSelected && (
+                    <Text style={styles.titleText} numberOfLines={1}>{item.title}</Text>
+                    {isSelected ? (
                       <Ionicons name="checkmark-circle" size={24} color="#4B0082" />
-                    )}
+                    ) : item.isPinned ? (
+                      <Ionicons name="pin" size={20} color="#4B0082" />
+                    ) : null
+                  }
                   </View>
                   <Text style={styles.contentText}>{item.content}</Text>
+                  <Text style={styles.cardDate}>
+                    {item.updatedAt?.toDate ? item.updatedAt.toDate().toLocaleDateString('id-ID'): ''}
+                  </Text>
                 </Pressable>
               );
             }}
           />
+          )
+        }
+          
           {!isSelectionMode && (
             <FAB
               style={[styles.fab, { backgroundColor: '#4B0082' }]}
@@ -216,7 +335,8 @@ const styles = StyleSheet.create({
     padding: 16,               // p-4
     marginBottom: 12,          // mb-3
     borderRadius: 16,          // rounded-2xl
-    borderWidth: 1,            // border
+    borderWidth: 1,
+    backgroundColor: 'white'        // border
   },
   cardNormal: {
     backgroundColor: 'white',  // bg-white
@@ -226,18 +346,53 @@ const styles = StyleSheet.create({
     backgroundColor: '#F3E8FF', // bg-purple-100 (kira-kira)
     borderColor: '#A855F7',     // border-purple-500 (kira-kira)
   },
+  card: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 12,
+    marginBottom: 15,
+    // Efek Bayangan (Shadow)
+    elevation: 2, // Android
+    shadowColor: '#000', // iOS
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    borderWidth: 1,
+    borderColor: '#eee'
+  },
   cardHeader: {
-    flexDirection: 'row',       // flex-row
-    justifyContent: 'space-between', // justify-between
-    alignItems: 'center',       // items-center
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+    flex: 1, // Agar tidak nabrak icon pin
+    marginRight: 10,
+  },
+  cardContent: {
+    fontSize: 14,
+    color: '#666',
+    lineHeight: 20,
+    marginBottom: 10,
+  },
+  cardDate: {
+    fontSize: 12,
+    color: '#999',
+    alignSelf: 'flex-end', // Rata kanan bawah
   },
   titleText: {
     fontWeight: 'bold',         // font-bold
     fontSize: 18,               // text-lg
-    color: '#000',              // default text color
+    color: '#000',    
+    flex: 1          // default text color
   },
   contentText: {
     color: '#6B7280',           // text-gray-500
-    marginTop: 4,               // mt-1
+    marginTop: 4,  
+    marginBottom: 10             // mt-1
   },
 });
--- app/add.tsx
+++ app/add.tsx
@@ -2,133 +2,196 @@ import { Colors } from '@/constants/theme';
 import { Ionicons } from '@expo/vector-icons';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { Text } from "react-native";
-// 1. Perbaiki spasi pada import
-import { saveOrUpdateNote, toggleArchiveNote, togglePinNote } from "@/service/notesServices";
-import { Platform, StatusBar, StyleSheet, TextInput, TouchableOpacity, View, useColorScheme } from 'react-native';
+import {
+    Alert // Tambahkan Alert untuk feedback
+    ,
+    Platform,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    useColorScheme
+} from 'react-native';
 import { Menu, MenuOption, MenuOptions, MenuTrigger } from 'react-native-popup-menu';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function HomeScreen() {
+// Pastikan nama file import ini SESUAI dengan nama file service kamu
+// Kalau file service kamu namanya 'firebaseConfig.ts' atau 'noteService.ts', sesuaikan di sini:
+import { saveOrUpdateNote, toggleArchiveNote, togglePinNote } from '@/service/notesServices';
+
+export default function NoteScreen() {
     const router = useRouter();
     const params = useLocalSearchParams();
     const colorScheme = useColorScheme();
 
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    const [ isPinned, setIsPinned ] = useState(false);
-    const [ isArchived, setIsArchived ] = useState(false);
+    
+    // State untuk UI (Icon berubah warna kalau aktif)
+    const [isPinned, setIsPinned] = useState(false);
+    const [isArchived, setIsArchived] = useState(false);
+    const [noteId, setNoteId] = useState<string | null>(null);
 
-    const [noteId, setNoteId] = useState<string | null>(null)
-
+    // 1. Ambil data jika ini mode Edit
     useEffect(() => {
         if (params.id) {
-            setNoteId(params.id as string)
-            setTitle(params.title as string || '')
-            setContent(params.content as string || '')
-
+            setNoteId(params.id as string);
+            setTitle(params.title as string || '');
+            setContent(params.content as string || '');
             setIsPinned(params.isPinned === 'true');
             setIsArchived(params.isArchived === 'true');
         }
-    },[params])
+    }, [params]);
 
+    // 2. Fungsi Simpan saat tombol Back ditekan
     const handleBack = async () => {
-        await saveOrUpdateNote(noteId, title, content)
-        router.back()
-    }
+        // Panggil service simpan
+        await saveOrUpdateNote(noteId, title, content);
+        router.back();
+    };
 
-    const handlePint = async() => {
-        if (!noteId) return
+    // 3. Fungsi Pin (Sematkan)
+    const handlePin = async () => {
+        if (!noteId) {
+            // Kalau catatan baru (belum ada ID), kita simpan dulu di state lokal/UI
+            // (Catatan: Service kamu saat ini men-save 'isPinned: false' untuk new note. 
+            // Nanti kita perlu update service agar mendukung pin saat create)
+            Alert.alert("Info", "Simpan catatan terlebih dahulu untuk menyematkan.");
+            return;
+        }
+        
         const result = await togglePinNote(noteId, isPinned);
         if (result?.success) {
-            // setIsPinned(result.newStatus);
+            // setIsPinned(result.newStatus); // Update icon di UI
         }
-    }
+    };
 
+    // 4. Fungsi Arsip
     const handleArchive = async () => {
-        if (!noteId) return
-        const result = await toggleArchiveNote(noteId, isArchived)
-        if (result?.success) {
-            router.back()
+        if (!noteId) {
+            Alert.alert("Info", "Simpan catatan terlebih dahulu untuk mengarsipkan.");
+            return;
         }
-    }
+        const result = await toggleArchiveNote(noteId, isArchived);
+        if (result?.success) {
+            // setIsArchived(result.newStatus);
+            // Opsional: Langsung kembali setelah arsip
+            router.back(); 
+        }
+    };
+
+    const themeTextColor = Colors[colorScheme ?? 'light'].text;
+
     return (
         <SafeAreaView style={styles.safeArea}>
             <View style={styles.header}>
                 <TouchableOpacity onPress={handleBack}>
-                    <Ionicons name="arrow-back" size={28} color="black" />
+                    <Ionicons name="arrow-back" size={28} color={themeTextColor} />
                 </TouchableOpacity>
+                
                 <View style={{ flex: 1 }} />
+                
+                {/* Menu Opsi */}
                 <Menu style={{ paddingRight: 15 }}>
                     <MenuTrigger>
-                        <Ionicons name="ellipsis-vertical" size={24} color="black" />
+                        <Ionicons name="ellipsis-vertical" size={24} color={themeTextColor} />
                     </MenuTrigger>
-                    <MenuOptions customStyles={{ optionsContainer: { marginTop: 40, borderRadius: 8 } }}>
-                        <MenuOption onSelect={handlePint}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
-                                <Ionicons name="pin-outline" size={20} color="#444" style={{ marginRight: 10 }} />
-                                <Text>Sematkan</Text>
+                    <MenuOptions customStyles={{ optionsContainer: styles.menuOptions }}>
+                        {/* Tombol Sematkan */}
+                        <MenuOption onSelect={handlePin}>
+                            <View style={styles.menuItem}>
+                                <Ionicons 
+                                    name={isPinned ? "pin" : "pin-outline"} 
+                                    size={20} 
+                                    color={isPinned ? "#4B0082" : "#444"} 
+                                    style={{ marginRight: 10 }} 
+                                />
+                                <Text style={{color: isPinned ? "#4B0082" : "#000"}}>
+                                    {isPinned ? "Lepas Sematan" : "Sematkan"}
+                                </Text>
                             </View>
                         </MenuOption>
 
+                        {/* Tombol Arsip */}
                         <MenuOption onSelect={handleArchive}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
-                                <Ionicons name="archive-outline" size={20} color="#444" style={{ marginRight: 10 }} />
-                                <Text>Arsip</Text>
+                            <View style={styles.menuItem}>
+                                <Ionicons 
+                                    name="archive-outline" 
+                                    size={20} 
+                                    color="#444" 
+                                    style={{ marginRight: 10 }} 
+                                />
+                                <Text>Arsipkan</Text>
                             </View>
                         </MenuOption>
                     </MenuOptions>
                 </Menu>
             </View>
-            <View style={{ backgroundColor: 'white', flex: 1, padding: 20 }}>
-                <View style={{ flex: 1 }}>
-                    <TextInput
-                        placeholder="Titlee"
-                        value={title}
-                        onChangeText={setTitle}
-                        style={{ height: 50, borderColor: 'gray', marginBottom: 10, paddingHorizontal: 10, fontSize: 20, fontWeight: 'bold', color: Colors[colorScheme ?? 'light'].text }}
-                    />
-                    <TextInput
-                        placeholder='Deskripsi'
-                        placeholderTextColor="#888"
-                        value={content}
-                        onChangeText={setContent}
-                        multiline={true}
-                        numberOfLines={4}
-                        style={{
-                            borderColor: 'gray',
-                            flex: 1,
-                            fontSize: 18,
-                            color: Colors[colorScheme ?? 'light'].text,
-                            paddingVertical: 10,
-                            textAlignVertical: 'top',
-                            paddingHorizontal: 10
-                        }}
-                    />
-                </View>
+
+            <View style={[styles.contentContainer, { backgroundColor: Colors[colorScheme ?? 'light'].background }]}>
+                <TextInput
+                    placeholder="Judul"
+                    placeholderTextColor="#999"
+                    value={title}
+                    onChangeText={setTitle}
+                    style={[styles.titleInput, { color: themeTextColor }]}
+                />
+                <TextInput
+                    placeholder="Ketik sesuatu..."
+                    placeholderTextColor="#999"
+                    value={content}
+                    onChangeText={setContent}
+                    multiline={true}
+                    style={[styles.contentInput, { color: themeTextColor }]}
+                />
             </View>
         </SafeAreaView>
-
-    )
+    );
 }
-
-// export default add
 
 const styles = StyleSheet.create({
     safeArea: {
         flex: 1,
-        backgroundColor: '#fff',
+        // Sesuaikan background dengan tema jika perlu
+        backgroundColor: '#fff', 
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     },
     header: {
         flexDirection: 'row',
         alignItems: 'center',
         paddingHorizontal: 10,
-        paddingVertical: 5,
-        paddingTop: 2,
-        paddingEnd: 2,
-        borderBottomColor: '#f0f0f0',
         height: 60,
-        justifyContent: 'space-between'
     },
-})
+    contentContainer: {
+        flex: 1,
+        padding: 20,
+    },
+    titleInput: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 15,
+    },
+    contentInput: {
+        flex: 1,
+        fontSize: 18,
+        textAlignVertical: 'top', // Agar teks mulai dari atas di Android
+    },
+    menuOptions: {
+        marginTop: 40,
+        borderRadius: 12,
+        padding: 5,
+        // Tambahkan shadow agar cantik
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+    },
+    menuItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10 
+    }
+});
